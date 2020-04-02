d9f029d5956daa93c90ed40152a25e0abcdd89f5
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-04-03 01:22:56 +0300

    polynom oop

---
+++ SixthQuestion/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SixthQuestion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(234, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(351, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(839, 65);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sonuç(x,a,like that) ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 508);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SixthQuestion/Poly.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixthQuestion
+{
+    class Poly
+    {
+        string equation;
+        public int a;
+        public void createpoly(int a)
+        {
+            if(a<2)
+            {
+                equation =  "Error";
+            }
+            if(a==2)
+            {
+                equation = "a*x*x+b*x+c";
+            }
+            if(a==3)
+            {
+                equation = "a*x*x*x+b*x*x+c*x+d";
+            }
+            if(a==4)
+            {
+                equation = "a*x*x*x*x+b*x*x*x+c*x*x+d*x+e";
+            }
+            if (a == 5)
+            {
+                equation = "a*x*x*x*x*x+b*x*x*x*x+c*x*x*x+d*x*x+e*x+f";
+            }
+                
+
+        }
+        public string equ()
+        {
+            createpoly(a);
+            return equation;
+        }
+            
+    }
+}
